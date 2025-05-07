7967d7f46300a7a1cab43ebe02e4652c74399535
brybryboi <brybryboi215@gmail.com>
2025-05-07 19:43:09 -0400

    correct mongo uri

--- boringApp/flask_app/config.py
+++ boringApp/flask_app/config.py
@@ -1,3 +1,3 @@
 # Stores all configuration values
 SECRET_KEY = 'os.urandom(16)'
-MONGODB_HOST = 'mongodb://localhost:27017/p4'
+MONGODB_HOST = 'mongodb+srv://brybryboi215:azpjIV4tjufxDher@cluster0.42g2bfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
