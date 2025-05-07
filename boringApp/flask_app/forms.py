ad8766e89d17f5e5907051a88b219a0a561b7650
brybryboi <brybryboi215@gmail.com>
2025-05-07 19:51:10 -0400

    forms

--- boringApp/flask_app/forms.py
+++ boringApp/flask_app/forms.py
@@ -3,33 +3,90 @@ from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from werkzeug.utils import secure_filename
-from wtforms import StringField, SubmitField, TextAreaField, PasswordField
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField, RadioField, SelectField
 from wtforms.validators import (
     InputRequired,
     Length,
     Email,
     EqualTo,
     ValidationError,
+    Optional,
 )
-
 
 from .models import User
 
-
-class SearchForm(FlaskForm):
-    search_query = StringField(
-        "Query", validators=[InputRequired(), Length(min=1, max=100)]
+# Home Page: Interests Form
+class InterestForm(FlaskForm):
+    # Which kind of query?
+    query_type = RadioField(
+        "What kind of activity?",
+        choices=[
+            ("random", "I'm bored—just give me something"),
+            ("filter", "I'm picky—help me filter (types & participants)"),
+        ],
+        validators=[InputRequired()],
     )
-    submit = SubmitField("Search")
 
+    # Filter options: type checkboxes
+    education = BooleanField("Education")
+    recreational = BooleanField("Recreational")
+    social = BooleanField("Social")
+    charity = BooleanField("Charity")
+    cooking = BooleanField("Cooking")
+    relaxation = BooleanField("Relaxation")
+    busywork = BooleanField("Busywork")
 
-class MovieReviewForm(FlaskForm):
-    text = TextAreaField(
-        "Comment", validators=[InputRequired(), Length(min=5, max=500)]
+    # Filter options: number of participants
+    participants = SelectField(
+        "Participants",
+        choices=[
+            ("", "Any"),
+            ("1", "1"),
+            ("2", "2"),
+            ("3", "3"),
+            ("4", "4"),
+            ("5", "5"),
+            ("6", "6"),
+            ("8", "8"),
+        ],
+        validators=[Optional()],
     )
-    submit = SubmitField("Enter Comment")
 
+    submit = SubmitField("Find Activity")
 
+    def validate_activity_key(self, activity_key):
+        if self.query_type.data == "key":
+            if not activity_key.data:
+                raise ValidationError("Please provide an activity key.")
+
+    def validate(self):
+        rv = super().validate()
+        if not rv:
+            return False
+
+        # If they chose filter, ensure they picked something
+        if self.query_type.data == "filter":
+            types_selected = any(
+                getattr(self, t).data
+                for t in [
+                    "education",
+                    "recreational",
+                    "social",
+                    "charity",
+                    "cooking",
+                    "relaxation",
+                    "busywork",
+                ]
+            )
+            if not types_selected and not self.participants.data:
+                self.query_type.errors.append(
+                    "Please select at least one activity type or participants."
+                )
+                return False
+
+        return True
+
+# Registration Page: Registration Form
 class RegistrationForm(FlaskForm):
     username = StringField(
         "Username", validators=[InputRequired(), Length(min=1, max=40)]
@@ -51,27 +108,21 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError("Email is taken")
 
-
-# TODO: implement fields
+# Login Page: Login Form
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
 
-
-# TODO: implement
+# Login Page: Changing current username
 class UpdateUsernameForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=1,max=40)])
     submit_username = SubmitField('Update')
 
-    # TODO: implement
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.objects(username=username.data).first()
             if user is not None:
                 raise ValidationError("Username is taken")
 
-# TODO: implement
-class UpdateProfilePicForm(FlaskForm):
-    picture = FileField('Profile Picture', validators=[FileRequired(), FileAllowed(['jpg', 'png'], "Images only!")])
-    submit_picture = SubmitField('Update')
+# Review Page for a Specific Activity: Review Form
